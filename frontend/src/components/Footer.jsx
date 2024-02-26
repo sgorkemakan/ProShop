db81f85c859c3d3c1d3ec06a206d36deef6c9c00
sgorkemakan <117218846+sgorkemakan@users.noreply.github.com>
2024-02-26 15:25:05 +0300

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,15 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">
-            <p>ProShop &copy; {currentYear}</p>
+              <p>
+              ProShop &copy; {currentYear} & Icon by
+              <a
+                style={{ textDecoration: "none", color: "grey" }}
+                href="https://www.flaticon.com/authors/ayub-irawan"
+              >
+                &nbsp;Ayub Irawan
+              </a>
+            </p>
           </Col>
         </Row>
       </Container>
